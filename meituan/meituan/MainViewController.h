eb43860f0fd8754a97873ae44d3c86ad873f2ceb
YYMasterGuoAo <873215689@qq.com>
2017-09-10 11:43:44 +0800

    创建主页

---
+++ meituan/meituan/MainViewController.h
@@ -0,0 +1,15 @@
+//
+//  MainViewController.h
+//  meituan
+//
+//  Created by 郭奥 on 2017/9/10.
+//  Copyright © 2017年 郭奥. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@property (nonatomic ,strong) NSMutableArray * array;
+
+@end
---
+++ meituan/meituan/MainViewController.m
@@ -0,0 +1,37 @@
+//
+//  MainViewController.m
+//  meituan
+//
+//  Created by 郭奥 on 2017/9/10.
+//  Copyright © 2017年 郭奥. All rights reserved.
+//
+
+#import "MainViewController.h"
+
+@interface MainViewController ()
+
+@end
+
+@implementation MainViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
