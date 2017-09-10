cc9f665384482adbbb6e2fd5e3db9fb85dc0e988
YYMasterGuoAo <873215689@qq.com>
2017-09-10 11:32:14 +0800

    初始化项目

---
+++ meituan/meituan/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  meituan
+//
+//  Created by 郭奥 on 2017/9/10.
+//  Copyright © 2017年 郭奥. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ meituan/meituan/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  meituan
+//
+//  Created by 郭奥 on 2017/9/10.
+//  Copyright © 2017年 郭奥. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
