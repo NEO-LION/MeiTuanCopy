b616eb179d99ca147ee9c99e46a2fe08d733d461
apple <apple@appledeMacBook-Air.local>
2017-09-12 15:52:50 +0800

    创建了人类

---
+++ meituan/meituan/Preson.h
@@ -0,0 +1,13 @@
+//
+//  Preson.h
+//  meituan
+//
+//  Created by apple on 17/9/12.
+//  Copyright © 2017年 郭奥. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Preson : NSObject
+
+@end
---
+++ meituan/meituan/Preson.m
@@ -0,0 +1,13 @@
+//
+//  Preson.m
+//  meituan
+//
+//  Created by apple on 17/9/12.
+//  Copyright © 2017年 郭奥. All rights reserved.
+//
+
+#import "Preson.h"
+
+@implementation Preson
+
+@end
